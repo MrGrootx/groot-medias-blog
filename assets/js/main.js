a5c598867ebcd5ecb9bd9a86357b079de32a4284
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-13 07:15:39 +0530

    add: nav

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*
+
+{
+-"source": {
+"id": null,
+"name": "Theregister.com"
+},
+"author": "Chris Williams",
+"title": "SEC Twitter hijacked to push fake news of ETF approval",
+"description": "Buy the hype, sell the, wait, what do we do now?!\nBreaking The SEC today said its Twitter account was hijacked to wrongly claim it had approved Bitcoin ETFs, causing cryptocurrency to spike and drop in price.…",
+"url": "https://www.theregister.com/2024/01/09/btc_sec/",
+"urlToImage": "https://regmedia.co.uk/2021/01/15/shutterstock_bitcoin.jpg",
+"publishedAt": "2024-01-09T21:48:07Z",
+"content": "Breaking The SEC today said its Twitter account was hijacked to wrongly claim it had approved the hotly anticipated Bitcoin ETFs, causing cryptocurrency to spike and then slip in price.\r\nIn a now-del… [+1092 chars]"
+},
+-{
+-"source": {
+"id": null,
+"name": "ReadWrite"
+},
+"author": "Brian-Damien Morgan",
+"title": "Insomniac leak: Rhysida hack sparks rally of support for game developer",
+"description": "Hacker group Rhysida made good on a promise to leak gaming and personal data related to Insomniac Games. This has […]\nThe post Insomniac leak: Rhysida hack sparks rally of support for game developer appeared first on ReadWrite.",
+"url": "https://readwrite.com/insomniac-leak-rhysida-hack-sparks-rally-of-support-for-game-developer/",
+"urlToImage": "https://readwrite.com/wp-content/uploads/2023/12/pexels-soumil-kumar-735911-scaled.jpg",
+"publishedAt": "2023-12-19T15:01:51Z",
+"content": "Hacker group Rhysida made good on a promise to leak gaming and personal data related to Insomniac Games. This has caused a rallying surge of support from game developers in the wake of these personal… [+2491 chars]"
+},
+
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+      Mallanna: ["Mallanna", "sans-serif"],
+    },
   },
   plugins: [],
-}
-
+};
